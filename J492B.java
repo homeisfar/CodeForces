1aac56205e64df83dd27baffeca67a6063317250
Ali Homafar <home.isfar@gmail.com>
2015-02-24 21:19:23 -0600

    Removed redundant list stuff. Don't know if it made the code actually any faster

--- J492B.java
+++ J492B.java
@@ -9,21 +9,15 @@ public class J492B
 	public static void main (String []args)
 	{
 		Scanner s = new Scanner(System.in);
-		String in = s.nextLine();
-		String read[] = in.split(" ");
 
-		int lanterns = Integer.parseInt(read[0]);
-		int length = Integer.parseInt(read[1]);
+		int lanterns = s.nextInt();
+		int length = s.nextInt();
 		int maxDistance = 0;
 		int x = 0;
-
-		in = s.nextLine();
-		read = in.split (" ");
-
 		ArrayList<Integer> al = new ArrayList<Integer>();
 
-		for (int i = 0; i < read.length; i++)
-			al.add(Integer.parseInt(read[i]));
+		for (int i = 0; i < lanterns; i++)
+			al.add(s.nextInt());
 		Collections.sort(al);
 
 		for (int i = 0; i < al.size()-1; i++)
@@ -35,7 +29,7 @@ public class J492B
 
 		double sol = maxDistance / 2.0;
 
-		if (sol < Math.abs(al.get(0)))
+		if (sol < al.get(0))
 			sol = al.get(0);
 
 		int finalD = Math.abs(al.get(al.size()-1)-length);
