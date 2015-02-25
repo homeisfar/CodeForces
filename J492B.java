4321bcd6b06d2ba87c249dfd533aa1cc4a15b564
Ali Homafar <home.isfar@gmail.com>
2015-02-24 21:37:37 -0600

    J492B changed ArrayList to array

--- J492B.java
+++ J492B.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 
 public class J492B
@@ -14,25 +14,25 @@ public class J492B
 		int length = s.nextInt();
 		int maxDistance = 0;
 		int x = 0;
-		ArrayList<Integer> al = new ArrayList<Integer>();
+		int []al = new int[lanterns];
 
 		for (int i = 0; i < lanterns; i++)
-			al.add(s.nextInt());
-		Collections.sort(al);
+			al[i] = s.nextInt();
+		Arrays.sort(al);
 
-		for (int i = 0; i < al.size()-1; i++)
+		for (int i = 0; i < al.length-1; i++)
 		{
-			x = Math.abs(al.get(i) - al.get(i+1));
+			x = Math.abs(al[i] - al[i+1]);
 			if( x > maxDistance)
 				maxDistance = x;
 		}
 
 		double sol = maxDistance / 2.0;
 
-		if (sol < al.get(0))
-			sol = al.get(0);
+		if (sol < al[0])
+			sol = al[0];
 
-		int finalD = Math.abs(al.get(al.size()-1)-length);
+		int finalD = Math.abs(al[al.length-1]-length);
 		if (sol < finalD)
 			sol = finalD;
 
